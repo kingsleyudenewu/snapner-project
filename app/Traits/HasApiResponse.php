de4c5a7940095510a340e11de9538c444d7cbe1d
kingsleyudenewu <kingzpacking@gmail.com>
2024-09-04 10:26:47 +0100

    Create api response traits and handle api exception errors

---
+++ app/Traits/HasApiResponse.php
@@ -0,0 +1,115 @@
+<?php
+
+namespace App\Traits;
+
+use Illuminate\Http\JsonResponse;
+
+trait HasApiResponse
+{
+    public function toJsonResponse(string $message, int $status, $data = null): JsonResponse
+    {
+        $isSuccessful = $status >= 100 && $status < 400;
+
+        $response = ["status" => $isSuccessful ? 'success' : 'error', 'message' => $message];
+
+        if (!empty($data)) {
+            $response[$isSuccessful ? 'data' : 'error'] = $data;
+        }
+
+        if ($data instanceof JsonResponse) {
+            $response = array_merge($response, (new JsonResponse())->setData($data)->getData(true));
+        }
+
+        return new JsonResponse($response, $status);
+    }
+
+    public function serverErrorResponse(string $message, int $status = 500, ?Throwable $exception = null): JsonResponse
+    {
+        if (null !== $exception) {
+            Log::error("{$exception->getMessage()} on line {$exception->getLine()} in {$exception->getFile()}");
+            report($exception);
+
+            $response['exception'] = $exception->getMessage();
+        }
+
+        return $this->toJsonResponse($message, $status, $response ?? null);
+    }
+
+    /**
+     * Set the validation error response alert.
+     *
+     * @param $data
+     *
+     * @return JsonResponse
+     */
+    public function formValidationErrorResponse($data = null): JsonResponse
+    {
+        return $this->toJsonResponse("Validation error", StatusResponse::HTTP_UNPROCESSABLE_ENTITY, $data);
+    }
+
+    /**
+     * Set the not found response alert.
+     *
+     * @param $message
+     * @param $data
+     *
+     * @return JsonResponse
+     */
+    public function notFoundResponse($message, $data = null): JsonResponse
+    {
+        return $this->toJsonResponse($message, StatusResponse::HTTP_NOT_FOUND, $data);
+    }
+
+    /**
+     * Set the success response alert.
+     *
+     * @param $message
+     * @param $data
+     *
+     * @return JsonResponse
+     */
+    public function successResponse($message, $data = null): JsonResponse
+    {
+        return $this->toJsonResponse($message, StatusResponse::HTTP_OK, $data);
+    }
+
+    /**
+     * Set the created resource response alert.
+     *
+     * @param string $message
+     * @param null $data
+     *
+     * @return JsonResponse
+     */
+    public function createdResponse(string $message, $data = null): JsonResponse
+    {
+        return $this->toJsonResponse($message, StatusResponse::HTTP_CREATED, $data);
+    }
+
+    /**
+     * Set the updated resource response alert.
+     *
+     * @param string $message
+     * @param null $data
+     *
+     * @return JsonResponse
+     */
+    public function forbiddenRequestResponse(string $message, $data = null): JsonResponse
+    {
+        return $this->toJsonResponse($message, StatusResponse::HTTP_FORBIDDEN, $data);
+    }
+
+    /**
+     * Set the bad request response alert.
+     *
+     * @param string $message
+     * @param null $data
+     *
+     * @return JsonResponse
+     */
+    public function badRequestAlert(string $message, $code = StatusResponse::HTTP_BAD_REQUEST, $data = null): JsonResponse
+    {
+        return $this->toJsonResponse($message, $code, $data);
+    }
+}
+
--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,12 +1,17 @@
 <?php
 
+use Illuminate\Auth\AuthenticationException;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Validation\UnauthorizedException;
+use Illuminate\Validation\ValidationException;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
+        api: __DIR__.'/../routes/api.php',
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
     )
@@ -14,5 +19,19 @@ return Application::configure(basePath: dirname(__DIR__))
         //
     })
     ->withExceptions(function (Exceptions $exceptions) {
-        //
+        $exceptions->render(function (NotFoundHttpException $e) {
+            return $this->notFoundResponse('Resource not found.');
+        });
+
+        $exceptions->render(function (ValidationException $e) {
+            return $this->formValidationErrorResponse($e->errors());
+        });
+
+        $exceptions->render(function (AuthenticationException $e) {
+            return $this->badRequestResponse($e->getMessage());
+        });
+
+        $exceptions->render(function (UnauthorizedException $e) {
+            return $this->badRequestResponse($e->getMessage());
+        });
     })->create();

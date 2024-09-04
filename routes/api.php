1f6db86f7e3a9bfccb281f199f477493f29fe578
kingsleyudenewu <kingzpacking@gmail.com>
2024-09-04 12:15:15 +0100

    Add route endpoints for user auth and projects with employee

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,43 @@
 <?php
 
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\EmployeeController;
+use App\Http\Controllers\ProjectController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::group(['prefix' => 'auth'], function () {
+    Route::post('/register', [AuthController::class, 'register'])->name('auth.register');
+    Route::post('/login', [AuthController::class, 'login'])->name('auth.login');
+    Route::post('/logout', [AuthController::class, 'logout'])->name('logout')->middleware('auth:sanctum');
+});
+
+Route::middleware('auth:sanctum')->group(function () {
+    // create a new route group for project controllers
+    Route::group([
+        'prefix' => 'projects',
+        'as' => 'projects.',
+    ],function () {
+        Route::get('/', [ProjectController::class, 'index'])->name('index');
+        Route::post('/store', [ProjectController::class, 'store'])->name('store');
+        Route::get('/{project}', [ProjectController::class, 'show'])->name('show');
+        Route::put('/{project}', [ProjectController::class, 'update'])->name('update');
+        Route::delete('/{project}', [ProjectController::class, 'destroy'])->name('destroy');
+    });
+
+    // create a new route group for employee controllers
+    Route::group([
+        'prefix' => 'employees',
+        'as' => 'employees.',
+    ],function () {
+        Route::get('/', [EmployeeController::class, 'index'])->name('index');
+        Route::post('/store', [EmployeeController::class, 'store'])->name('store');
+        Route::get('/{employee}', [EmployeeController::class, 'show'])->name('show');
+        Route::put('/{employee}', [EmployeeController::class, 'update'])->name('update');
+        Route::delete('/{employee}', [EmployeeController::class, 'destroy'])->name('destroy');
+    });
+});

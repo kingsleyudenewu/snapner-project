e3efbe91bbf9f379c293b2b9ce1e6e76d645d071
kingsleyudenewu <kingzpacking@gmail.com>
2024-09-04 13:03:16 +0100

    Add owner id to projects

--- database/migrations/2024_09_04_092945_create_projects_table.php
+++ database/migrations/2024_09_04_092945_create_projects_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
     {
         Schema::create('projects', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('owner_id')->constrained('users');
             $table->string('name')->unique();
             $table->text('description');
             $table->string('status')->default(StatusEnum::PENDING->value);

3c75cd91982e8be523ad58e25fafbcb63c07c434
kingsleyudenewu <kingzpacking@gmail.com>
2024-09-04 11:12:52 +0100

    update employee data and add validation

--- app/Data/EmployeeData.php
+++ app/Data/EmployeeData.php
@@ -8,7 +8,23 @@ class EmployeeData extends Data
 {
     public function __construct(
         public string $name,
+        public int $project_id,
         public string $email,
-        public string $phone_number,
+        public string $position,
     ) {}
+
+    public static function rules(): array
+    {
+        return [
+            'name' => ['required', 'string', 'max:255'],
+            'project_id' => ['required', 'integer', 'exists:projects,id'],
+            'email' => ['required', 'email', 'max:255'],
+            'position' => ['required', 'string', 'max:255'],
+        ];
+    }
+
+    public static function stopOnFirstFailure(): bool
+    {
+        return true;
+    }
 }
--- app/Http/Resources/EmployeeResource.php
+++ app/Http/Resources/EmployeeResource.php
@@ -14,6 +14,8 @@ class EmployeeResource extends JsonResource
      */
     public function toArray(Request $request): array
     {
-        return parent::toArray($request);
+        return [
+
+        ];
     }
 }

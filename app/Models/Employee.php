4054ab3cb08729fe930fd4ec3918d514ebd3164f
kingsleyudenewu <kingzpacking@gmail.com>
2024-09-04 13:04:04 +0100

    Add Notifiable trait for employee to receive emails

--- app/Models/Employee.php
+++ app/Models/Employee.php
@@ -5,10 +5,12 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Notifications\Notifiable;
 
 class Employee extends Model
 {
     use HasFactory;
+    use Notifiable;
     use SoftDeletes;
 
     protected $guarded = ['id'];
